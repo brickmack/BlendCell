d66964b4d6a96c205e9121879a650172f1d6dde6
Mackenzie Crawford <brickmack@gmail.com>
2019-04-25 13:36:30 -0400

    v0.4
    * Remove deprecated RunSimCombined class
    
    * Number of simulation steps is no longer hard-coded, saved in custom property of the parent object and can be set via slider in RunSim
    
    * Add tool to reset simulation
    
    * Add tool to set selected cells to be alive
    
    * Add tool to add predefined patterns (needs work. Currently only supports gliders, and only at a predefined point)
    
    * Cells are now all instances of a single cell. Vastly faster and uses less memory (on a mid-range laptop, now comfortably supports a 2d grid of 75x75 cells. Previously topped out around 20x20), and allows the user to easily change the mesh of all at once
    
    * General optimizations to BuildGrid now that many functionalities have been moved to other tools
    
    * Refactoring

--- BlendCell.py
+++ BlendCell.py
@@ -1,7 +1,7 @@
 bl_info = {
 	"name": "BlendCell",
 	"author": "Mackenzie Crawford",
-	"version": (0, 3),
+	"version": (0, 4),
 	"blender": (2, 79, 0),
 	"description": "Visualization tool for cellular automata",
 	"warning": "",
@@ -10,54 +10,69 @@ bl_info = {
 
 import bpy
 import re
+import time
 
 def stepForward(Matrix, n, t):
-    newMatrix = [[0 for x in range(n)] for y in range(n)] #stores born/live/die = 1/0/-1 for each cell in the next step, NOT a reference to the cells themselves
-    
-    for i in range(n):
-        for j in range(n):
-            #compute 8-neighbor sum using toroidal boundary conditions - x and y wrap around so that the simulaton takes place on a toroidal surface
-            total = 0
-            if Matrix[i][(j-1)%n].pass_index == 1:
-                total = total + 1
-            if Matrix[i][(j+1)%n].pass_index == 1:
-                total = total + 1
-            if Matrix[(i-1)%n][j].pass_index == 1:
-                total = total + 1
-            if Matrix[(i+1)%n][j].pass_index == 1:
-                total = total + 1
-            if Matrix[(i-1)%n][(j-1)%n].pass_index == 1:
-                total = total + 1
-            if Matrix[(i-1)%n][(j+1)%n].pass_index == 1:
-                total = total + 1
-            if Matrix[(i+1)%n][(j-1)%n].pass_index == 1:
-                total = total + 1
-            if Matrix[(i+1)%n][(j+1)%n].pass_index == 1:
-                total = total + 1
-        
-            #apply conways rules
-            if Matrix[i][j].pass_index == 1:
-                if (total < 2) or (total > 3):
-                    #die
-                    newMatrix[i][j] = -1
-            else:
-                if total == 3:
-                    #birth
-                    newMatrix[i][j] = 1
-            #otherwise, newMatrix[i][j] = 0/continue living
+	newMatrix = [[0 for x in range(n)] for y in range(n)] #stores born/live/die = 1/0/-1 for each cell in the next step, NOT a reference to the cells themselves
 
-    #now we have the next set of cell states, apply them to the cells themselves
-    for i in range(0, n):
-        for j in range(0, n):
-            if newMatrix[i][j] == 1:
-                Matrix[i][j].pass_index = 1
-            elif newMatrix[i][j] == -1:
-                Matrix[i][j].pass_index = 0
-    
-    #set next keyframe for all cells
-    for x in range(0, n):
-        for y in range(0, n):
+	for i in range(n):
+		for j in range(n):
+			#compute 8-neighbor sum using toroidal boundary conditions - x and y wrap around so that the simulaton takes place on a toroidal surface
+			total = 0
+			if Matrix[i][(j-1)%n].pass_index == 1:
+				total = total + 1
+			if Matrix[i][(j+1)%n].pass_index == 1:
+				total = total + 1
+			if Matrix[(i-1)%n][j].pass_index == 1:
+				total = total + 1
+			if Matrix[(i+1)%n][j].pass_index == 1:
+				total = total + 1
+			if Matrix[(i-1)%n][(j-1)%n].pass_index == 1:
+				total = total + 1
+			if Matrix[(i-1)%n][(j+1)%n].pass_index == 1:
+				total = total + 1
+			if Matrix[(i+1)%n][(j-1)%n].pass_index == 1:
+				total = total + 1
+			if Matrix[(i+1)%n][(j+1)%n].pass_index == 1:
+				total = total + 1
+        
+			#apply conways rules
+			if Matrix[i][j].pass_index == 1:
+				if (total < 2) or (total > 3):
+					#die
+					newMatrix[i][j] = -1
+			else:
+				if total == 3:
+					#birth
+					newMatrix[i][j] = 1
+			#otherwise, newMatrix[i][j] = 0/continue living
+
+	#now we have the next set of cell states, apply them to the cells themselves
+	for i in range(0, n):
+		for j in range(0, n):
+			if newMatrix[i][j] == 1:
+				Matrix[i][j].pass_index = 1
+			elif newMatrix[i][j] == -1:
+				Matrix[i][j].pass_index = 0
+
+	#set next keyframe for all cells
+	keyframeAll(Matrix, n, t)
+			
+def keyframeAll(Matrix, count, t):
+	#set next keyframe for all cells
+    for x in range(0, count):
+        for y in range(0, count):
             Matrix[x][y].keyframe_insert(data_path="pass_index", frame=t)
+			
+def rebuildMatrix(children, count):
+	#these may be out of order, so we need to place them back in a 2d array with the same structure as the original
+	Matrix = [[0 for x in range(count)] for y in range(count)]
+	for child in children:
+		coords = [int(s) for s in re.findall(r'\d+', child.name)]
+		
+		Matrix[coords[0]][coords[1]] = child
+		
+	return Matrix
             
 def findMaterial():
     #first check if a BlendCell material is already set
@@ -109,7 +124,7 @@ def createMaterial():
     
     return mat
 	
-class BuildGrid(bpy.types.Operator):
+class BuildGridOP(bpy.types.Operator):
 	bl_idname = "object.build_grid"
 	bl_label = "Build grid"
 	bl_options = {'REGISTER', 'UNDO'}
@@ -131,42 +146,89 @@ class BuildGrid(bpy.types.Operator):
 		parent["count"] = self.count
 		parent["cellWidth"] = self.cubeWidth
 		parent["gapWidth"] = self.gap
-
+		parent["simulationSteps"] = 50 #default
 		mat = findMaterial()
-
-		Matrix = [[0 for x in range(self.count)] for y in range(self.count)]
-		occupied = [[7, 7], [8,6], [8,5], [7,5], [6,5]] #glider
-
+		
+		#set initial cell, since we need a non-instanced object
+		x = -offset
+		y = -offset
+		z = 0
+		
+		bpy.ops.mesh.primitive_cube_add(location=(x,y,z), radius=self.cubeWidth/2)
+		origCell = bpy.context.active_object
+		origCell.name = "Cell 0, 0"
+		origCell.data.materials.append(mat)
+		originalMeshData = origCell.data
+		
+		#for the rest of the cells, we create them as instances of the original
 		for xIndex in range(0, self.count):
 			x = (xIndex) * (self.cubeWidth + self.gap) - offset
 			for yIndex in range(0, self.count):
 				y = (yIndex) * (self.cubeWidth + self.gap) - offset
-				z = 0
-				bpy.ops.mesh.primitive_cube_add(location=(x,y,z), radius=self.cubeWidth/2)
-				bpy.context.active_object.name = "Cell " + str(xIndex) + ", " + str(yIndex)
-				Matrix[xIndex][yIndex] = bpy.context.active_object
 				
-				#set material
-				Matrix[xIndex][yIndex].data.materials.append(mat)
-				Matrix[xIndex][yIndex].pass_index = 0
-			
-				#set parent
-				Matrix[xIndex][yIndex].parent = parent
-
-		#highlight initial live cells
-		for pair in occupied:
-			Matrix[pair[0]][pair[1]].pass_index = 1
-
-		#set initial keyframe for all cells
-		for x in range(0, self.count):
-			for y in range(0, self.count):
-				Matrix[x][y].keyframe_insert(data_path="pass_index", frame=0)
+				newObject = bpy.data.objects.new(("Cell " + str(xIndex) + ", " + str(yIndex)), originalMeshData)
+				newObject.location = (x,y,z)
+				bpy.context.scene.objects.link(newObject)
+				
+				newObject.parent = parent
 		
 		return {'FINISHED'}
 		
-class RunSimOP(bpy.types.Operator):
-	bl_idname = "object.run_sim"
-	bl_label = "Run simulation"
+class AddPatternOP(bpy.types.Operator):
+	bl_idname = "object.add_pattern"
+	bl_label = "Add pattern"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	@classmethod
+	def poll(cls, context):
+		selected = bpy.context.selected_objects
+		if (selected is not None) and (len(selected) == 1) and (selected[0].type == "EMPTY"):
+			return True
+		
+		return False
+		
+	def execute(self, context):
+		#get the cells (children of the parent empty) and data (custom properties of the empty)
+		selected = bpy.context.selected_objects
+		parent = selected[0]
+		count = parent["count"]
+		
+		Matrix = rebuildMatrix(parent.children, count)
+			
+		#add selected pattern at center
+		occupied = [[7, 7], [8,6], [8,5], [7,5], [6,5]] #glider
+		
+		#highlight initial live cells
+		for pair in occupied:
+			Matrix[pair[0]][pair[1]].pass_index = 1
+		
+		return {'FINISHED'}
+		
+class SetLiveOP(bpy.types.Operator):
+	bl_idname = "object.set_live"
+	bl_label = "Set cells alive"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	@classmethod
+	def poll(cls, context):
+		selected = bpy.context.selected_objects
+		if (selected is not None):
+			return True
+			
+		return False
+		
+	def execute(self, context):
+		#set all the selected cells alive
+		selected = bpy.context.selected_objects
+		
+		for cell in selected:
+			cell.pass_index = 1
+		
+		return {'FINISHED'}
+
+class ResetSimOP(bpy.types.Operator):
+	bl_idname = "object.reset_sim"
+	bl_label = "Reset simulation"
 	bl_options = {'REGISTER', 'UNDO'}
 	
 	@classmethod
@@ -178,93 +240,69 @@ class RunSimOP(bpy.types.Operator):
 		return False
 	
 	def execute(self, context):
+		#get the cells (children of the parent empty)
+		selected = bpy.context.selected_objects
+		parent = selected[0]
+		children = parent.children
+		
+		for child in children:
+			#clear all keyframes
+			child.animation_data_clear()
+			
+		return {'FINISHED'}
+		
+class RunSimOP(bpy.types.Operator):
+	bl_idname = "object.run_sim"
+	bl_label = "Run simulation"
+	bl_options = {'REGISTER', 'UNDO'}
+	
+	simulationSteps = bpy.props.IntProperty(name="Steps", default=50, min=0, max=2147483647)
+	
+	@classmethod
+	def poll(cls, context):
+		selected = bpy.context.selected_objects
+		if (selected is not None) and (len(selected) == 1) and (selected[0].type == "EMPTY"):
+			return True
+		
+		return False
+		
+	def invoke(self, context, event):
+		selected = bpy.context.selected_objects[0]
+		self.simulationSteps = selected["simulationSteps"]
+		
+		return self.execute(context)
+	
+	def execute(self, context):
 		#get the cells (children of the parent empty) and data (custom properties of the empty)
 		selected = bpy.context.selected_objects
 		parent = selected[0]
 		count = parent["count"]
 		
-		#get all children of the parent empty
-		children = parent.children
-			
-		#these may be out of order, so we need to place them back in a 2d array with the same structure as the original
-		Matrix = [[0 for x in range(count)] for y in range(count)]
-		for child in children:
-			coords = [int(s) for s in re.findall(r'\d+', child.name)]
-			
-			Matrix[coords[0]][coords[1]] = child
-				
-		#now that we have a properly formed matrix, we can start the simulation itself
-		for step in range(1, 10):
+		#update the simulationSteps custom property in the parent object
+		parent["simulationSteps"] = self.simulationSteps
+		
+		Matrix = rebuildMatrix(parent.children, count)
+		
+		#set first keyframe for all cells
+		keyframeAll(Matrix, count, 0)
+		
+		#start the simulation
+		for step in range(1, self.simulationSteps):
 			stepForward(Matrix, count, step)
 		
 		return {'FINISHED'}
-	
-class RunSimCombinedOP(bpy.types.Operator):
-	bl_idname = "object.run_sim_combined"
-	bl_label = "Run simulation COMBINED"
-	bl_options = {'REGISTER', 'UNDO'}
-	
-	def execute(self, context):
-		#hardcoded for testing
-		count = 12
-		cubeWidth = 2
-		gap = 1
-		steps = 50
-	
-		offset = count * (cubeWidth + gap) / 2 - cubeWidth + (gap/2)
-
-		#create parent empty
-		parent = bpy.data.objects.new("Parent", None)
-		bpy.context.scene.objects.link(parent)
-		parent.empty_draw_size = 2
-		parent.empty_draw_type = 'PLAIN_AXES'
-
-		mat = findMaterial()
-
-		Matrix = [[0 for x in range(count)] for y in range(count)]
-		occupied = [[7, 7], [8,6], [8,5], [7,5], [6,5]] #glider
-
-		for xIndex in range(0, count):
-			x = (xIndex) * (cubeWidth + gap) - offset
-			for yIndex in range(0, count):
-				y = (yIndex) * (cubeWidth + gap) - offset
-				z = 0
-				bpy.ops.mesh.primitive_cube_add(location=(x,y,z), radius=cubeWidth/2)
-				bpy.context.active_object.name = "Cell " + str(xIndex) + ", " + str(yIndex)
-				Matrix[xIndex][yIndex] = bpy.context.active_object
-				
-				#set material
-				Matrix[xIndex][yIndex].data.materials.append(mat)
-				Matrix[xIndex][yIndex].pass_index = 0
-			
-				#set parent
-				Matrix[xIndex][yIndex].parent = parent
-
-		#highlight initial live cells
-		for pair in occupied:
-			Matrix[pair[0]][pair[1]].pass_index = 1
-
-		#set initial keyframe for all cells
-		for x in range(0, count):
-			for y in range(0, count):
-				Matrix[x][y].keyframe_insert(data_path="pass_index", frame=0)
-
-		for step in range(1, steps):
-			stepForward(Matrix, count, step)
-			
-		return {'FINISHED'}
+		
+classes = (RunSimOP, ResetSimOP, BuildGridOP, AddPatternOP, SetLiveOP)
 
 def register():
 	from bpy.utils import register_class
-	register_class(RunSimCombinedOP)
-	register_class(RunSimOP)
-	register_class(BuildGrid)
+	for cls in classes:
+		register_class(cls)
 	
 def unregister():
 	from bpy.utils import unregister_class
-	unregister_class(RunSimCombinedOP)
-	unregister_class(RunSimOP)
-	unregister_class(BuildGrid)
+	for cls in reverse(classes):
+		unregister_class(cls)
 
 if __name__ == "__main__":
 	register()
--- README.md
+++ README.md
@@ -2,37 +2,59 @@
 
 Blender python addon for visualization of cellular automata.
 
-Version 0.2
+Version 0.4
 
 ## Usage:
 
-Currently only available as a script, must be manually run and parameters are all hard-coded. Cells are represented as cubes. Cell aliveness is indicated by object pass_index controlling a mix shader node in a material. Generates keyframes so it can be rendered as an animation (currently you MUST render it to view, doesn't work in the viewport)
+Cells are represented as cubes. Cell aliveness is indicated by object pass_index controlling a mix shader node in a material. Generates keyframes so it can be rendered as an animation (currently you MUST render it to view, doesn't work in the viewport)
+
+Use "Build grid" to generate the board. Select desired live cells, then use "Set cells alive". Then use "Run simulation" and view the results (must render to see board state at time t)
 
 ## Planned features:
 
 * 2d and 3d versions
 
-* configurable rules (currently uses the standard Conway's Game of Life rules)
+* Configurable rules (currently uses the standard Conway's Game of Life rules)
 
-* multiple boundary handling modes (infinite grid with finite/infinite display, non-crossable boundary, toroidal projection on finite grid). Currently only toroidal projection is supported
-
-* Multiple cell shape options
+* Multiple boundary handling modes (infinite grid with finite/infinite display, non-crossable boundary, toroidal projection on finite grid). Currently only toroidal projection is supported
 
 * Grid lines
 
 * Highlight newly added/killed cells
 
-* Visually set initial state instead of hardcoded array of live cells
+* Blender 2.80 support
 
-* Automatically clear previous board when simulation is re-run
+* Ability to add certain standard patterns (glider, etc)
+
+* Set starting frame using the timeline, and set location of the overall grid using the 3d cursor
 
 ## Known issues:
 
 * Keyframing is not optimal, adds unneccessary keyframes to cells which have not changed
 
-* Matrix reconstruction in RunSimOP is not optimal. Should store coordinates in custom object properties. Safer, much faster
+* Matrix reconstruction process is not optimal. Should store coordinates in custom object properties. Safer, much faster
+
+* BuildGrid creates a duplicate for cell 0,0 because of the instancing. User can manually remove this.
 
 ## Changelog:
+
+### 0.4:
+
+* Remove deprecated RunSimCombined class
+
+* Number of simulation steps is no longer hard-coded, saved in custom property of the parent object and can be set via slider in RunSim
+
+* Add tool to reset simulation
+
+* Add tool to set selected cells to be alive
+
+* Add tool to add predefined patterns (needs work. Currently only supports gliders, and only at a predefined point)
+
+* Cells are now all instances of a single cell. Vastly faster and uses less memory (on a mid-range laptop, now comfortably supports a 2d grid of 75x75 cells. Previously topped out around 20x20), and allows the user to easily change the mesh of all at once
+
+* General optimizations to BuildGrid now that many functionalities have been moved to other tools
+
+* Refactoring
 
 ### 0.3:
 
